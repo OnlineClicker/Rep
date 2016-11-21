1714c4ad81c6cd65d65ec9a184f9cf1e63c53353
ggesus <denys.khavin@hs-osnabrueck.de>
2016-11-21 14:33:35 +0100

    dada

--- Test/src/gitTest.java
+++ Test/src/gitTest.java
@@ -4,8 +4,11 @@ public class gitTest {
 	public static void main(String[] args) {
 		
 		int i=1;
-		
+		String h="hi";
 		System.out.println(i);
+		System.out.println(h);
+		
+		
 		
 		// TODO Auto-generated method stub
 
