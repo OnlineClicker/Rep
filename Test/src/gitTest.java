1c150cfa605c5ecc0ffe2b461e296253cbca918a
TorbenKoehne <torben.koehne@hs-osnabrueck.de>
2016-11-22 14:00:00 +0100

    Revert "geordnet"

---
+++ Test/src/gitTest.java
@@ -0,0 +1,19 @@
+
+public class gitTest {
+
+	public static void main(String[] args) {
+		
+		int i=1;
+		String h="hi";
+		System.out.println(i);
+		System.out.println(h);
+		
+		String arberZekaj = "Yeah";
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
